e6360ad0104ff7a5f30ad81db2ca299ff3c98149
Amit Raikwar <amitraikwar993613@gmail.com>
2026-07-08 23:27:10 +0530

    code

--- 10_lecture/server.js
+++ 10_lecture/server.js
@@ -1,7 +1,6 @@
-const http=require('http');
-http.createServer((req,res)=>{
-    if(req.url==='/'){
-        res.write('Hello World');
-        res.end();
-    }
-});
+const http = require('http');
+const user = { name: 'John', age: 30, city: 'New York' };
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(user));
+}).listen(5000);
